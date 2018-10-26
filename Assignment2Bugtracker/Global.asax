eec158db0e461f5cff84732f68c48e2d139130ee
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-10-26 10:17:01 -0500

    new project added

---
+++ Assignment2Bugtracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment2Bugtracker.MvcApplication" Language="C#" %>
---
+++ Assignment2Bugtracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2Bugtracker.Startup))]
+namespace Assignment2Bugtracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
